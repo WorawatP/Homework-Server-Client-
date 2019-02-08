2ffec027ecd289372e3eb71092693f336c4307e0
WorawatP <heartbeetbug@gmail.com>
2019-02-08 14:18:16 +0700

    Complete Code

--- client1.js
+++ client1.js
@@ -1,8 +1,8 @@
 var net = require('net')
-var HOST = '127.0.0.1'
+var HOST = '127.0.0.2'
 var PORT = 6969
 var client = new net.Socket()
-var num = 0
+var num = Math.floor(Math.random() * 21)
 var i = 1
 client.connect(PORT, HOST, function(){
     console.log('connected to : ' + HOST + ':' + PORT)
@@ -11,24 +11,18 @@ client.connect(PORT, HOST, function(){
 
 client.on('data', function(data){
     console.log('DATA : ' + data)
-    if(data == 'OK'){
-        console.log('DATA : ' + data)
-        client.write('' + num)
-    }
-    else if(i < 5){
-        if(data == 'WRONG'){
+    if(i <= 5 && data == "WRONG"){
             client.write('' + num)
             num = Math.floor(Math.random() * 21)
             console.log('Send :' + num)
             console.log('i:' + i)
             i++
-        }
-        else if(data == 'BINGO'){
+    }
+    else if(data == 'BINGO'){
             client.destroy()
         }
-    }
     else{
-        client.destroy()
+    	    client.destroy() 
     }
 })
 
